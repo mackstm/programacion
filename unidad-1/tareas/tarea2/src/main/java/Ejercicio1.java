a2bf696e8add7e67117d54d4e88b27c803b4f2fb
mackstm <thelewyntm@gmail.com>
2023-09-28 15:53:49 +0100

    please help

--- unidad-1/tareas/tarea2/src/main/java/Ejercicio1.java
+++ unidad-1/tareas/tarea2/src/main/java/Ejercicio1.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+
 /**
- * @author Nombre del autor
+ * @author jmboada
+ */
+
+/**
+ * Desarrolle un algoritmo que permita leer tres valores y almacenarlos en las variables A, B y C respectivamente.
+ * El algoritmo debe imprimir cual es el mayor y cual es el menor.
+ * Recuerde constatar que los tres valores introducidos por el teclado sean valores distintos.
+ * Presente un mensaje de alerta en caso de que se detecte la introducción de valores iguales.
  */
 public class Ejercicio1 {
 
     /**
      * Descripcion del algoritmo
-     * aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+     * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
