7ab92aa4b8af6d9d6f7f1b51c1890ec84b465296
mackstm <thelewyntm@gmail.com>
2023-09-28 15:40:31 +0100

    damn

---
+++ unidad-1/tareas/tarea2/src/main/java/Ejercicio1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * @author Nombre del autor
+ */
+public class Ejercicio1 {
+
+    /**
+     * Descripcion del algoritmo
+     * aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
