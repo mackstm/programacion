4547445e2bb23a55d40c9d5757a398e115469125
mackstm <thelewyntm@gmail.com>
2023-10-02 16:52:40 +0100

    a

--- unidad-1/tareas/Tarea1/src/main/java/ies/puerto/Ejercicio8.java
+++ unidad-1/tareas/Tarea1/src/main/java/ies/puerto/Ejercicio8.java
@@ -7,23 +7,28 @@ import java.util.Scanner;
  */
 public class Ejercicio8 {
     public static void main(String[] args){
-        int nAsignaturas;
+        int nAsignaturas; // Inicializacion de variable donde almacenaremos el numero de asignaturas
 
+        // Peticion de nAsignaturas por teclado
         Scanner read = new Scanner(System.in);
         System.out.println("Ingrese el número de asignaturas: ");
         nAsignaturas = read.nextInt();
 
+        // Inicializacion de sum y el contador
         float sum = 0;
         int i = 0;
 
+        // Bucle que acumula sum. Si el número de asignaturas es 0, se lo salta
         while (nAsignaturas != 0 && i < nAsignaturas) {
             System.out.println("Introduzca la nota de la asignatura " + (i + 1));
             sum += read.nextFloat();
             i++;
         }
 
+            // Calculo de la media
             float media = sum / nAsignaturas;
 
+            // Se imprime la media por pantalla
             System.out.println("Nota media: " + media);
     }
 }
