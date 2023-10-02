3ba0e69ed81241c4b7b998dd34af9106a56b311b
mackstm <thelewyntm@gmail.com>
2023-10-02 15:21:22 +0100

    a

--- unidad-1/tareas/Tarea1/src/main/java/ies/puerto/Ejercicio8.java
+++ unidad-1/tareas/Tarea1/src/main/java/ies/puerto/Ejercicio8.java
@@ -1,12 +1,32 @@
 package ies.puerto;
 
+import java.util.Scanner;
+
 /**
  * Desarrolle un algoritmo que permita calcular
  * Promedio de Notas; finaliza cuando N = 0.
  */
 public class Ejercicio8 {
     public static void main(String[] args){
-        
+        int n;
+        do {
+            Scanner read = new Scanner (System.in);
+
+            System.out.println("Introduzca el número de asignaturas: ");
+            n = read.nextInt();
+            System.out.println("Número de asignaturas: " + n);
+
+            float sum = 0;
+            for (int i = 0; i < n; i++) {
+                System.out.println("Introduzca la nota de la asignatura " + (i + 1));
+                sum += read.nextFloat();
+            }
+
+            float media = sum / n;
+
+            System.out.println("Nota media: " + media);
+
+        }while (n != 0);
     }
 
 }
