b0df411a0d602b18e296358f4508d74393cb3bc7
mackstm <thelewyntm@gmail.com>
2023-11-06 18:48:17 +0000

    a

--- unidad-1/tareas/trabajo-clases/src/main/java/ies/puerto/Student.java
+++ unidad-1/tareas/trabajo-clases/src/main/java/ies/puerto/Student.java
@@ -12,4 +12,62 @@ public class Student {
     private int age;
     private String degree;
     private float averageGrades;
+
+    /**
+     * Default constructor
+     */
+    public Student() {}
+
+    /**
+     * Constructor with name, age, degree and average parameters
+     * @param name of student
+     * @param age of student
+     * @param degree of student
+     * @param averageGrades of student
+     */
+    public Student(String name, int age, String degree, float averageGrades) {
+        this.name = name;
+        this.age = age;
+        this.degree = degree;
+        this.averageGrades = averageGrades;
+    }
+
+    /**
+     * Getters and setters
+     */
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public float getAverageGrades() {
+        return averageGrades;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void setAverageGrades(float averageGrades) {
+        this.averageGrades = averageGrades;
+    }
+
+    public String printDetails() {
+        return "Name: " + name + "\nAge: " + "\nDegree: " + degree + "\nAverage of grades: " + averageGrades;
+    }
+
+    public boolean isPassed() {
+        return averageGrades >= 5;
+    }
 }
--- unidad-1/tareas/trabajo-clases/src/test/java/ies/puerto/StudentTest.java
+++ unidad-1/tareas/trabajo-clases/src/test/java/ies/puerto/StudentTest.java
@@ -1,4 +1,25 @@
 package ies.puerto;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
+    @Test
+    public void detailsTestOK() {
+        Student student = new Student("Maxi", 23, "DAM", 7.7f);
+        String result = student.printDetails();
+        String resultOK = "Name: Maxi\nAge: 23\nDegree: DAM\nAverage of grades: 7.7";
+        Assertions.assertEquals(resultOK, result);
+    }
+
+    @Test
+    public void passingGradeTestOK() {
+
+    }
+
+    @Test
+    public void failingGradeTestOK() {
+
+    }
+
 }
