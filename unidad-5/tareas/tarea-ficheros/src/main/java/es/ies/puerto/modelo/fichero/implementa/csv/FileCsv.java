fcde813a601ba68caaa823cd35f2952e7415f52b
mackstm <thelewyntm@gmail.com>
2024-02-23 18:29:49 +0000

    a

--- unidad-5/tareas/tarea-ficheros/src/main/java/es/ies/puerto/modelo/fichero/implementa/csv/FileCsv.java
+++ unidad-5/tareas/tarea-ficheros/src/main/java/es/ies/puerto/modelo/fichero/implementa/csv/FileCsv.java
@@ -1,9 +1,23 @@
 package es.ies.puerto.modelo.fichero.implementa.csv;
 
+import es.ies.puerto.modelo.entity.Alimento;
+import es.ies.puerto.modelo.fichero.abstractas.FicheroAbstract;
+
+import java.util.List;
+
 /**
  * Clase para trabajar con ficheros CSV
  * @author Jose Maximiliano Boada Martin
  */
-public class FileCsv {
+public class FileCsv extends FicheroAbstract {
 
+    @Override
+    public List<Alimento> lecturaAlimento() {
+        return null;
+    }
+
+    @Override
+    public String escrituraAlimento() {
+        return null;
+    }
 }
