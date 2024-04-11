761502a9099098037dc084b4c836ce1e3c3b4dfb
mackstm <thelewyntm@gmail.com>
2024-04-11 14:43:47 +0100

    a

---
+++ unidad-6/recursividad/src/test/java/es/ies/puerto/Ejercicio3.java
@@ -0,0 +1,18 @@
+package es.ies.puerto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Ejercicio3 {
+    Ejercicio3 ejercicio;
+
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+    @Test
+    public void ejercicio1Test() {
+
+    }
+}
