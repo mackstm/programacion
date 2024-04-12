fd6d9f214527029d806410a73af6590582a3cdfe
mackstm <thelewyntm@gmail.com>
2024-04-12 18:37:43 +0100

    a

--- unidad-7/personajes-marvel-bbdd/src/main/resources/marvel.sql
+++ unidad-7/personajes-marvel-bbdd/src/main/resources/marvel.sql
@@ -7,19 +7,29 @@ CREATE TABLE IF NOT EXISTS Personaje (
 
 CREATE TABLE IF NOT EXISTS Poder (
     poderId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    poder TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PersonajePoder (
     personajeId INTEGER NOT NULL,
-    poder TEXT NOT NULL,
-    FOREIGN KEY(personajeId) REFERENCES Personajes(personajeId)
+    poderId INTEGER NOT NULL,
+    FOREIGN KEY(personajeId) REFERENCES Personaje(personajeId),
+    FOREIGN KEY(poderId) REFERENCES Poder(poderId),
+    PRIMARY KEY(personajeId, poderId)
 );
 
 INSERT INTO Personaje (nombre, alias, genero) VALUES
     ('Iron Man', 'Tony Stark', 'Masculino'),
     ('Spider-Man', 'Peter Parker', 'Masculino');
 
-INSERT INTO Poder (personajeId, poder) VALUES
-    (1, 'Vuelo'),
-    (1, 'Armadura tecnologica avanzada'),
-    (1, 'Rayos laser'),
-    (2, 'Agilidad sobrehumana'),
-    (2, 'Trepamuros'),
-    (2, 'Sentido aracnido');
+INSERT INTO Poder (poder) VALUES
+    ('Vuelo'),
+    ('Armadura tecnologica avanzada'),
+    ('Rayos laser'),
+    ('Agilidad sobrehumana'),
+    ('Trepamuros'),
+    ('Sentido aracnido');
+
+INSERT INTO PersonajePoder VALUES
+(1, 1), (1, 2), (1, 3),
+(2, 4), (2, 5), (2, 6);
